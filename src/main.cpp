ce104177c54ad60ec0f7e3404af7f74ade41ab1e
Airton Silva <silvaairton03@gmail.com>
2025-04-01 10:19:18 -0400

    TS Fuzzy com 5 funções de pertinência

--- src/main.cpp
+++ src/main.cpp
@@ -57,10 +57,11 @@ const float wheelRadius = 0.0325;
 float k[4] = {-39.5113,   -3.0546,  -10.5095,   -7.4827}; //Ts = 0.008s
 
 float K1[4] = {-39.7073,   -3.4122,  -10.6711,   -7.6583};
-// float K2[4] = {-71.0149,   -6.2501,  -11.4300,   -8.2029};
-// float K3[4] = {-71.0149,   -6.2501,  -11.4300,   -8.2029};
-float K2[4] = { -49.6606,   -4.8163,  -14.4073,  -10.3396};
-float K3[4] = { -49.6606,   -4.8163,  -14.4073,  -10.3396};
+float K2[4] = { -49.6606,   -4.8163,  -14.4073,  -10.3396}; //30 graus
+float K3[4] = { -49.6606,   -4.8163,  -14.4073,  -10.3396}; //-30 graus
+float K4[4] = {-71.0149,   -6.2501,  -11.4300,   -8.2029}; //45 graus
+float K5[4] = {-71.0149,   -6.2501,  -11.4300,   -8.2029};//-45 gruas
+
 float Ts = 8;
 float M = 0.208;
 
@@ -142,68 +143,80 @@ void loop() {
   pendulumPosition = stepperStates.getRobotPosition();
   pendulumVelocity = stepperStates.getRobotVelocity();
 
+
   float sigma = radians(20);
   float mf1 = exp(-pow((theta - radians(0)) / sigma, 2));
-  float mf2 = exp(-pow((theta - radians(45)) / sigma, 2));
-  float mf3 = exp(-pow((theta + radians(45)) / sigma, 2));
+  float mf2 = exp(-pow((theta - radians(30)) / sigma, 2));
+  float mf3 = exp(-pow((theta + radians(30)) / sigma, 2));
+  float mf4 = exp(-pow((theta - radians(45)) / sigma, 2));
+  float mf5 = exp(-pow((theta + radians(45)) / sigma, 2));
 
-  float total = mf1 + mf2 + mf3;
+  // float total = mf1 + mf2 + mf3;
+  float total = mf1 + mf2 + mf3 + mf4 + mf5;
   float h1 = mf1 / total;
   float h2 = mf2 / total;
   float h3 = mf3 / total;
+  float h4 = mf4 / total;
+  float h5 = mf5 / total;
 
   float x[4] = {theta, fusedThetaRate, pendulumPosition, pendulumVelocity};
 
   // Auto reset if robot is vertically balanced and stable
-  if (fabs(theta) < 0.02 && fabs(fusedThetaRate) < 0.05) {
-    pendulumPosition = 0;
-    pendulumVelocity = 0;
-  }
+  // if (fabs(theta) < 0.02 && fabs(fusedThetaRate) < 0.05) {
+  //   pendulumPosition = 0;
+  //   pendulumVelocity = 0;
+  // }
 
   float u = 0.0;
   if (timerdiff >= Ts) {
     stepperStates.update();
 
-    // if (fabs(theta) < SAFE_ANGLE) {
-    //   // Controle Fuzzy TS com 3 MF
-    //   for (int i = 0; i < 4; i++) {
-    //     float Ki = h1 * K1[i] + h2 * K2[i] + h3 * K3[i];
-    //     u += -Ki * x[i];
-    //   }
+    if (fabs(theta) < SAFE_ANGLE) {
+      // Controle Fuzzy TS com 3 MF
+      // for (int i = 0; i < 4; i++) {
+      //   float Ki = h1 * K1[i] + h2 * K2[i] + h3 * K3[i];
+      //   u += -Ki * x[i];
+      // }
 
-    //   float Fm = u / 2.0;
-    //   float a = Fm / M;
-    //   float vel = lastVel + a * (Ts / 1000.0);
+      //Para cinco funções de pertinência
+      for (int i = 0; i < 4; i++) {
+        float Ki = h1 * K1[i] + h2 * K2[i] + h3 * K3[i] + h4 * K4[i] + h5 * K5[i];
+        u += -Ki * x[i];
+      }
 
-    //   if (fabs(vel) > 1.0) vel = (vel > 0) ? 1.0 : -1.0;
+      float Fm = u / 2.0;
+      float a = Fm / M;
+      float vel = lastVel + a * (Ts / 1000.0);
 
-    //   float controlSteps = (vel * STEPS_PER_REVOLUTION) / (2 * PI * wheelRadius);
+      if (fabs(vel) > 1.0) vel = (vel > 0) ? 1.0 : -1.0;
 
-    //   if (fabs(controlSteps) > MAX_STEPS) {
-    //     controlSteps = (controlSteps > 0) ? MAX_STEPS : -MAX_STEPS;
-    //   }
+      float controlSteps = (vel * STEPS_PER_REVOLUTION) / (2 * PI * wheelRadius);
 
-    //   stepperLeft.setSpeed(controlSteps);
-    //   stepperRight.setSpeed(-controlSteps);
+      if (fabs(controlSteps) > MAX_STEPS) {
+        controlSteps = (controlSteps > 0) ? MAX_STEPS : -MAX_STEPS;
+      }
 
-    //   lastVel = vel;
-    // } else {
-    //   stepperLeft.setSpeed(0);
-    //   stepperRight.setSpeed(0);
-    //   lastVel = 0;
-    //   u = 0;
-    // }
+      stepperLeft.setSpeed(controlSteps);
+      stepperRight.setSpeed(-controlSteps);
+
+      lastVel = vel;
+    } else {
+      stepperLeft.setSpeed(0);
+      stepperRight.setSpeed(0);
+      lastVel = 0;
+      u = 0;
+    }
 
     prevMillis = currentMillis;
   }
-  Serial.print(1.60); Serial.print(",");
-  Serial.print(theta); Serial.print(",");
+  // Serial.print(1.60); Serial.print(",");
+  Serial.print(fusedThetaRate); Serial.print(",");
   Serial.print(thetaRate); Serial.print(",");
-  Serial.println(-1.60);
   // Serial.print(",");
-  // Serial.print(pendulumPosition); Serial.print(",");
-  // Serial.print(pendulumVelocity); Serial.print(",");
-  // Serial.println(u);
+  Serial.print(pendulumPosition); Serial.print(",");
+  Serial.print(pendulumVelocity); Serial.print(",");
+  Serial.println(u);
+  // Serial.println(-1.60);
 }
 
 
@@ -213,43 +226,6 @@ void IRAM_ATTR onTimerLeft() {
 
 void IRAM_ATTR onTimerRight() {
   stepperRight.runSpeed();
-}
-
-void computeMembership(float theta, float &h1, float &h2, float &h3)
-{
-  float sigma = radians(20);
-  float mu1 = exp(-pow((theta - radians(0)) / sigma, 2));
-  float mu2 = exp(-pow((theta - radians(45)) / sigma, 2));
-  float mu3 = exp(-pow((theta + radians(45)) / sigma, 2));
-
-  float total = mu1 + mu2 + mu3;
-  h1 = mu1 / total;
-  h2 = mu2 / total;
-  h3 = mu3 / total;
-}
-
-void updateStates()
-{
-  imu.update();
-  theta = imu.getIMUAngleY();
-  fusedThetaRate = imu.getFusedRadSpeed();
-  pendulumPosition = stepperStates.getRobotPosition();
-  pendulumVelocity = stepperStates.getRobotVelocity();
-}
-
-float computeTScontrol()
-{
-  float h1, h2, h3;
-  computeMembership(theta, h1, h2, h3);
-
-  float state[4] = {theta, fusedThetaRate, pendulumPosition, pendulumVelocity};
-  float u = 0.0;
-
-  for (int i = 0; i < 4; i++) {
-    float Ki = h1 * K1[i] + h2 * K2[i] + h3 * K3[i];
-    u += -Ki * state[i];
-  }
-  return u;
 }
 
 float yawAS5600(float leftAngle, float rightAngle)
@@ -262,17 +238,3 @@ float yawAS5600(float leftAngle, float rightAngle)
   return yawMagEncoder;
 }
 
-void applyMotorCommand(float u) {
-  float Fm = u / 2.0f;
-  float a = Fm / M;
-  float vel = lastVel + a * (Ts / 1000.0f);
-
-  float controlSteps = (vel * STEPS_PER_REVOLUTION) / (2.0f * PI * wheelRadius);
-
-  controlSteps = constrain(controlSteps, -MAX_STEPS, MAX_STEPS);
-
-  stepperLeft.setSpeed(controlSteps);
-  stepperRight.setSpeed(-controlSteps);
-
-  lastVel = vel;
-}
